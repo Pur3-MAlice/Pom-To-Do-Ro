e5e2864b933c2cc4a351b2adb8bce6230b2f004c
Pur3-MAlice <aliceford98@live.co.uk>
2023-11-25 19:04:50 +0000

    adds authentication and authorised user only functionality to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,17 +3,18 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    # is_owner = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
     # posts_count = serializers.ReadOnlyField()
     # followers_count = serializers.ReadOnlyField()
 
-    # def get_is_owner(self, obj):
-    #     request = self.context['request']
-    #     return request.user == obj.owner
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'id', 'owner', 'is_owner',
+            'created_at', 'updated_at', 'name',
         ]
