8a9b5c2439d0e4a0764f2160917e11dc80c24ecf
Pur3-MAlice <aliceford98@live.co.uk>
2023-11-26 17:20:58 +0000

    adds django testing to habits, tests.py all pass

--- habits/models.py
+++ habits/models.py
@@ -20,24 +20,24 @@ class Habit(models.Model):
     def __str__(self):
         return f'{self.id} {self.title}'
 
-def current_streak(habit):
-    total_streak = 0
-    current_streak = 0
-    today = datetime.date.today()
-    compareDate = today + datetime.timedelta(1)
-    entry_dates = list(Entry.objects.values("date").filter(habit=habit, date__lte = today).order_by("-date"))
+# def current_streak(habit):
+#     total_streak = 0
+#     current_streak = 0
+#     today = datetime.date.today()
+#     compareDate = today + datetime.timedelta(1)
+#     entry_dates = list(Entry.objects.values("date").filter(habit=habit, date__lte = today).order_by("-date"))
 
-    for date in entry_dates:
-        delta = compareDate - date
-        if delta.days == 1: 
-            current_streak += 1
-        elif delta.days == 0: 
-            pass
-        else: 
-            break
-        compareDate = date
+#     for date in entry_dates:
+#         delta = compareDate - date
+#         if delta.days == 1: 
+#             current_streak += 1
+#         elif delta.days == 0: 
+#             pass
+#         else: 
+#             break
+#         compareDate = date
 
-    if current_streak > total_streak:
-        total_streak = current_streak
+#     if current_streak > total_streak:
+#         total_streak = current_streak
 
-    return total_streak
+#     return total_streak
--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Habit
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class HabitListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='susan', password='pass')
+
+    def test_logged_in_user_can_create_habit(self):
+        self.client.login(username='susan', password='pass')
+        response = self.client.post('/habits/', {'title': 'a title'})
+        count = Habit.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+
+    def test_user_not_logged_in_cant_create_task(self):
+        response = self.client.post('/habits/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class HabitDetailViewTests(APITestCase):
+    def setUp(self):
+        susan = User.objects.create_user(username='susan', password='pass')
+        brian = User.objects.create_user(username='brian', password='pass')
+        Habit.objects.create(
+            owner=susan, title='a title'
+        )
+        Habit.objects.create(
+            owner=brian, title='another title'
+        )
+
+    def test_can_retrieve_habit_using_valid_id(self):
+        response = self.client.get('/habits/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_habit_using_invalid_id(self):
+        response = self.client.get('/habits/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_habit(self):
+        self.client.login(username='susan', password='pass')
+        response = self.client.put('/habits/1/', {'title': 'a new title', 'completed': 'False'})
+        habit = Habit.objects.filter(pk=1).first()
+        self.assertEqual(habit.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_habit(self):
+        self.client.login(username='susan', password='pass')
+        response = self.client.put('/habits/2/', {'title': 'a new title', 'completed': 'True'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
