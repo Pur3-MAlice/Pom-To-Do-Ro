c2a737d08f836f99461854e01082ce68f9b3e1cf
Pur3-MAlice <aliceford98@live.co.uk>
2023-11-26 12:22:31 +0000

    removes form/serializer  elements from habit

--- habits/serializers.py
+++ habits/serializers.py
@@ -18,6 +18,5 @@ class HabitSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'created_at', 'updated_at', 'title', 'completed',
-            'last_completed_date', 'chain_count'
         ]
 
