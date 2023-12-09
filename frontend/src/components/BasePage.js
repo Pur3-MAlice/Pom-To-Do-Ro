d6ecce925b0cfdb1fcbcd44d7c746db51f541aa5
Pur3-MAlice <aliceford98@live.co.uk>
2023-12-09 12:37:26 +0000

    makes the BasePage reactive to mointer size

--- frontend/src/components/BasePage.js
+++ frontend/src/components/BasePage.js
@@ -3,7 +3,6 @@ import { Row, Col, Button } from "react-bootstrap";
 import NavBar from "./NavBar";
 
 import TaskManager from "../pages/tasks/TaskManager";
-// import PomodoroTimer from '../pages/timer/PomodoroTimer';
 import HabitsTracker from "../pages/habits/HabitsTracker";
 import CreateTask from "../pages/tasks/CreateTask";
 
@@ -20,10 +19,10 @@ const BasePage = () => {
   return (
     <>
       <Row noGutters className={styles.BasePage}>
-        <Col sm={12} md={4} style={{ height: "415px", padding: "0" }}>
+        <Col sm={12} md={4} style={{ height: "70vh", padding: "0", border: '1px solid red'}}>
             <Timer />
         </Col>
-        <Col sm={12} md={8} style={{ height: "635px", padding: "0" }}>
+        <Col sm={12} md={8} style={{ height: "90vh", padding: "0", border: '1px solid red' }}>
           <Button className={`${styles.Button} ${styles.Blue} ${styles.ButtonTask}`} variant="primary" onClick={handleToggle}>
             Add Task
           </Button>
@@ -31,10 +30,10 @@ const BasePage = () => {
         </Col>
       </Row>
       <Row noGutters className={styles.BasePage}>
-        <Col sm={12} md={4}>
+        <Col sm={12} md={4} className={styles.HabitBox} style={{ height: "30vh", padding: "0", border: '1px solid red' }}>
           <HabitsTracker />
         </Col>
-        <Col sm={12} md={8} className={styles.NavBox}>
+        <Col sm={12} md={8} className={styles.NavBox} style={{ height: "10vh", padding: "0", border: '1px solid red' }}>
           <NavBar />
         </Col>
       </Row>
